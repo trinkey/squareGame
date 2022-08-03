25cb6d54c993080192a3a1200e8e667e5e7f5473
trinkey_ <97406176+trinkey@users.noreply.github.com>
2022-08-03 17:11:26 -0400

    Added ability to restart
    press r to restart

--- squareGameBalaced.py
+++ squareGameBalaced.py
@@ -1,93 +1,99 @@
 import turtle, random, time
 
-score = 0
-up = 0
-down = 0
-left = 0
-right = 0
+def reset():
+    global left, right, up, down, h, z, score
 
-screen = turtle.Screen()
-screen.tracer(0)
-screen.setup(640, 640, 0, 0)
-screen.bgcolor("#444444")
+    screen.onkey(None, "space")
+    screen.onkey(None, "r")
 
-player = turtle.Turtle()
-player.speed(0)
-player.shape("square")
-player.color("lime")
-player.pu()
-player.goto(0, 0)
-player.shapesize(2, 2, None)
+    player.speed(0)
+    player.shape("square")
+    player.color("lime")
+    player.pu()
+    player.goto(0, 0)
+    player.shapesize(2, 2, None)
 
-enemy1 = turtle.Turtle()
-enemy1.speed(0)
-enemy1.shape("square")
-enemy1.color("red")
-enemy1.pu()
-enemy1.goto(random.randint(-310, 310), random.randint(320, 960))
+    enemy1.speed(0)
+    enemy1.shape("square")
+    enemy1.color("red")
+    enemy1.pu()
+    enemy1.goto(random.randint(-310, 310), random.randint(320, 960))
 
-enemy2 = turtle.Turtle()
-enemy2.speed(0)
-enemy2.shape("square")
-enemy2.color("red")
-enemy2.pu()
-enemy2.goto(random.randint(-310, 310), random.randint(320, 960))
+    enemy2.speed(0)
+    enemy2.shape("square")
+    enemy2.color("red")
+    enemy2.pu()
+    enemy2.goto(random.randint(-310, 310), random.randint(320, 960))
 
-enemy3 = turtle.Turtle()
-enemy3.speed(0)
-enemy3.shape("square")
-enemy3.color("red")
-enemy3.pu()
-enemy3.goto(random.randint(-310, 310), random.randint(320, 960))
+    enemy3.speed(0)
+    enemy3.shape("square")
+    enemy3.color("red")
+    enemy3.pu()
+    enemy3.goto(random.randint(-310, 310), random.randint(320, 960))
 
-enemy4 = turtle.Turtle()
-enemy4.speed(0)
-enemy4.shape("square")
-enemy4.color("red")
-enemy4.pu()
-enemy4.goto(random.randint(-310, 310), random.randint(320, 960))
+    enemy4.speed(0)
+    enemy4.shape("square")
+    enemy4.color("red")
+    enemy4.pu()
+    enemy4.goto(random.randint(-310, 310), random.randint(320, 960))
 
-enemy5 = turtle.Turtle()
-enemy5.speed(0)
-enemy5.shape("square")
-enemy5.color("red")
-enemy5.pu()
-enemy5.goto(random.randint(-310, 310), random.randint(320, 960))
+    enemy5.speed(0)
+    enemy5.shape("square")
+    enemy5.color("red")
+    enemy5.pu()
+    enemy5.goto(random.randint(-310, 310), random.randint(320, 960))
 
-enemy6 = turtle.Turtle()
-enemy6.speed(0)
-enemy6.shape("square")
-enemy6.color("red")
-enemy6.pu()
-enemy6.goto(random.randint(-310, 310), random.randint(320, 960))
+    enemy6.speed(0)
+    enemy6.shape("square")
+    enemy6.color("red")
+    enemy6.pu()
+    enemy6.goto(random.randint(-310, 310), random.randint(320, 960))
 
-enemy7 = turtle.Turtle()
-enemy7.speed(0)
-enemy7.shape("square")
-enemy7.color("red")
-enemy7.pu()
-enemy7.goto(random.randint(-310, 310), random.randint(320, 960))
+    enemy7.speed(0)
+    enemy7.shape("square")
+    enemy7.color("red")
+    enemy7.pu()
+    enemy7.goto(random.randint(-310, 310), random.randint(320, 960))
 
-enemy8 = turtle.Turtle()
-enemy8.speed(0)
-enemy8.shape("square")
-enemy8.color("red")
-enemy8.pu()
-enemy8.goto(random.randint(-310, 310), random.randint(320, 960))
+    enemy8.speed(0)
+    enemy8.shape("square")
+    enemy8.color("red")
+    enemy8.pu()
+    enemy8.goto(random.randint(-310, 310), random.randint(320, 960))
 
-enemy9 = turtle.Turtle()
-enemy9.speed(0)
-enemy9.shape("square")
-enemy9.color("red")
-enemy9.pu()
-enemy9.goto(random.randint(-310, 310), random.randint(320, 960))
+    enemy9.speed(0)
+    enemy9.shape("square")
+    enemy9.color("red")
+    enemy9.pu()
+    enemy9.goto(random.randint(-310, 310), random.randint(320, 960))
 
-enemy0 = turtle.Turtle()
-enemy0.speed(0)
-enemy0.shape("square")
-enemy0.color("red")
-enemy0.pu()
-enemy0.goto(random.randint(-310, 310), random.randint(320, 960))
+    enemy0.speed(0)
+    enemy0.shape("square")
+    enemy0.color("red")
+    enemy0.pu()
+    enemy0.goto(random.randint(-310, 310), random.randint(320, 960))
+
+    score = 0
+    up = 0
+    down = 0
+    left = 0
+    z = 10
+    h = 0
+    right = 0
+
+def showTurtles():
+    player.showturtle()
+    player.clear()
+    enemy1.showturtle()
+    enemy2.showturtle()
+    enemy3.showturtle()
+    enemy4.showturtle()
+    enemy5.showturtle()
+    enemy6.showturtle()
+    enemy7.showturtle()
+    enemy8.showturtle()
+    enemy9.showturtle()
+    enemy0.showturtle()
 
 def moveEnemiesDown(amount):
     enemy1.goto(enemy1.xcor(),enemy1.ycor() - amount)
@@ -188,6 +194,46 @@ def playerUpR():
 def die():
     screen.bye()
 
+def death():
+    enemy1.hideturtle()
+    enemy2.hideturtle()
+    enemy3.hideturtle()
+    enemy4.hideturtle()
+    enemy5.hideturtle()
+    enemy6.hideturtle()
+    enemy7.hideturtle()
+    enemy8.hideturtle()
+    enemy9.hideturtle()
+    enemy0.hideturtle()
+    player.hideturtle()
+
+    player.goto(0,0)
+    player.color("#ffffff")
+    t = "Game Over!! Score: " + str(score) + "!"
+    player.write(t, align = "center", font = ["Arial", 30, "normal"])
+
+    screen.onkey(die, "space")
+    screen.onkey(reset, "r")
+
+    screen.update()
+
+player = turtle.Turtle()
+enemy1 = turtle.Turtle()
+enemy2 = turtle.Turtle()
+enemy3 = turtle.Turtle()
+enemy4 = turtle.Turtle()
+enemy5 = turtle.Turtle()
+enemy6 = turtle.Turtle()
+enemy7 = turtle.Turtle()
+enemy8 = turtle.Turtle()
+enemy9 = turtle.Turtle()
+enemy0 = turtle.Turtle()
+
+screen = turtle.Screen()
+screen.tracer(0)
+screen.setup(640, 640, 0, 0)
+screen.bgcolor("#444444")
+
 screen.onkeypress(playerUp, "w")
 screen.onkeypress(playerLeft, "a")
 screen.onkeypress(playerDown, "s")
@@ -209,38 +255,23 @@ screen.onkeyrelease(playerDownR, "Down")
 screen.onkeyrelease(playerRightR, "Right")
 
 screen.listen()
-h = False
 
-z = 10
 while True:
-    moveEnemiesDown(round(z))
-    checkForMoveUp()
-    playerMovement(round(z / 2) + 5)
-    h = checkForCollision()
-    if h == 1:
-        break
-    screen.update()
-    time.sleep(1 / 60)
-    z += 0.01
-
-enemy1.hideturtle()
-enemy2.hideturtle()
-enemy3.hideturtle()
-enemy4.hideturtle()
-enemy5.hideturtle()
-enemy6.hideturtle()
-enemy7.hideturtle()
-enemy8.hideturtle()
-enemy9.hideturtle()
-enemy0.hideturtle()
-player.hideturtle()
-
-player.goto(0,0)
-player.color("#ffffff")
-t = "Game Over!! Score: " + str(score) + "!"
-player.write(t, align = "center", font = ["Arial", 30, "normal"])
-
-screen.onkey(die, "space")
-
-screen.update()
-screen.mainloop()
+    reset()
+    while h == 0:
+        showTurtles()
+        moveEnemiesDown(round(z))
+        checkForMoveUp()
+        playerMovement(round(z / 2) + 5)
+        h = checkForCollision()
+        if h == 1:
+            death()
+        screen.update()
+        time.sleep(1 / 60)
+        z += 0.01
+    while h == 1:
+        time.sleep(1 / 20)
+        try:
+            screen.update()
+        except:
+            pass
