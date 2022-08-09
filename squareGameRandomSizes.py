64de8227cd5fe9cdcfd7f8009305b6be4bcf2b7b
trinkey_ <97406176+trinkey@users.noreply.github.com>
2022-08-09 17:22:42 -0400

    Create squareGameRandomSizes.py

---
+++ squareGameRandomSizes.py
@@ -0,0 +1,316 @@
+import turtle, random, time
+
+closed = 0
+score = -10
+def reset():
+    global left, right, up, down, h, z, score
+
+    screen.onkey(None, "space")
+    screen.onkey(None, "r")
+
+    player.speed(0)
+    player.shape("square")
+    player.color("lime")
+    player.pu()
+    player.goto(0, 0)
+    player.shapesize(2, 2, None)
+
+    enemy1.speed(0)
+    enemy1.shape("square")
+    enemy1.color("red")
+    enemy1.pu()
+    enemy1.goto(random.randint(-310, 310), -500)
+
+    enemy2.speed(0)
+    enemy2.shape("square")
+    enemy2.color("red")
+    enemy2.pu()
+    enemy2.goto(random.randint(-310, 310), -500)
+
+    enemy3.speed(0)
+    enemy3.shape("square")
+    enemy3.color("red")
+    enemy3.pu()
+    enemy3.goto(random.randint(-310, 310), -500)
+
+    enemy4.speed(0)
+    enemy4.shape("square")
+    enemy4.color("red")
+    enemy4.pu()
+    enemy4.goto(random.randint(-310, 310), -500)
+
+    enemy5.speed(0)
+    enemy5.shape("square")
+    enemy5.color("red")
+    enemy5.pu()
+    enemy5.goto(random.randint(-310, 310), -500)
+
+    enemy6.speed(0)
+    enemy6.shape("square")
+    enemy6.color("red")
+    enemy6.pu()
+    enemy6.goto(random.randint(-310, 310), -500)
+
+    enemy7.speed(0)
+    enemy7.shape("square")
+    enemy7.color("red")
+    enemy7.pu()
+    enemy7.goto(random.randint(-310, 310), -500)
+
+    enemy8.speed(0)
+    enemy8.shape("square")
+    enemy8.color("red")
+    enemy8.pu()
+    enemy8.goto(random.randint(-310, 310), -500)
+
+    enemy9.speed(0)
+    enemy9.shape("square")
+    enemy9.color("red")
+    enemy9.pu()
+    enemy9.goto(random.randint(-310, 310), -500)
+
+    enemy0.speed(0)
+    enemy0.shape("square")
+    enemy0.color("red")
+    enemy0.pu()
+    enemy0.goto(random.randint(-310, 310), -500)
+
+    score = -10
+    up = 0
+    down = 0
+    left = 0
+    z = 10
+    h = 0
+    right = 0
+
+def showTurtles():
+    player.showturtle()
+    player.clear()
+    enemy1.showturtle()
+    enemy2.showturtle()
+    enemy3.showturtle()
+    enemy4.showturtle()
+    enemy5.showturtle()
+    enemy6.showturtle()
+    enemy7.showturtle()
+    enemy8.showturtle()
+    enemy9.showturtle()
+    enemy0.showturtle()
+
+def moveEnemiesDown(amount):
+    enemy1.goto(enemy1.xcor(),enemy1.ycor() - amount)
+    enemy2.goto(enemy2.xcor(),enemy2.ycor() - amount)
+    enemy3.goto(enemy3.xcor(),enemy3.ycor() - amount)
+    enemy4.goto(enemy4.xcor(),enemy4.ycor() - amount)
+    enemy5.goto(enemy5.xcor(),enemy5.ycor() - amount)
+    enemy6.goto(enemy6.xcor(),enemy6.ycor() - amount)
+    enemy7.goto(enemy7.xcor(),enemy7.ycor() - amount)
+    enemy8.goto(enemy8.xcor(),enemy8.ycor() - amount)
+    enemy9.goto(enemy9.xcor(),enemy9.ycor() - amount)
+    enemy0.goto(enemy0.xcor(),enemy0.ycor() - amount)
+
+def checkForMoveUp():
+    global score, enemy0Size, enemy1Size, enemy2Size, enemy3Size, enemy4Size, enemy5Size, enemy6Size, enemy7Size, enemy8Size, enemy9Size
+    if enemy1.ycor() <= -320 - enemy1Size:
+        enemy1Size = random.randint(10, 30)
+        enemy1.goto(random.randint(-320 + enemy1Size, 320 - enemy1Size), random.randint(360 + enemy1Size, 1400 + enemy1Size))
+        enemy1.turtlesize(enemy1Size / 10, enemy1Size / 10)
+        score += 1
+    if enemy2.ycor() <= -320 - enemy2Size:
+        enemy2Size = random.randint(10, 30)
+        enemy2.goto(random.randint(-320 + enemy2Size, 320 - enemy2Size), random.randint(360 + enemy2Size, 1400 + enemy2Size))
+        enemy2.turtlesize(enemy2Size / 10, enemy2Size / 10)
+        score += 1
+    if enemy3.ycor() <= -320 - enemy3Size:
+        enemy3Size = random.randint(10, 30)
+        enemy3.goto(random.randint(-320 + enemy3Size, 320 - enemy3Size), random.randint(360 + enemy3Size, 1400 + enemy3Size))
+        enemy3.turtlesize(enemy3Size / 10, enemy3Size / 10)
+        score += 1
+    if enemy4.ycor() <= -320 - enemy4Size:
+        enemy4Size = random.randint(10, 30)
+        enemy4.goto(random.randint(-320 + enemy4Size, 320 - enemy4Size), random.randint(360 + enemy4Size, 1400 + enemy4Size))
+        enemy4.turtlesize(enemy4Size / 10, enemy4Size / 10)
+        score += 1
+    if enemy5.ycor() <= -320 - enemy5Size:
+        enemy5Size = random.randint(10, 30)
+        enemy5.goto(random.randint(-320 + enemy5Size, 320 - enemy5Size), random.randint(360 + enemy5Size, 1400 + enemy5Size))
+        enemy5.turtlesize(enemy5Size / 10, enemy5Size / 10)
+        score += 1
+    if enemy6.ycor() <= -320 - enemy6Size:
+        enemy6Size = random.randint(10, 30)
+        enemy6.goto(random.randint(-320 + enemy6Size, 320 - enemy6Size), random.randint(360 + enemy6Size, 1400 + enemy6Size))
+        enemy6.turtlesize(enemy6Size / 10, enemy6Size / 10)
+        score += 1
+    if enemy7.ycor() <= -320 - enemy7Size:
+        enemy7Size = random.randint(10, 30)
+        enemy7.goto(random.randint(-320 + enemy7Size, 320 - enemy7Size), random.randint(360 + enemy7Size, 1400 + enemy7Size))
+        enemy7.turtlesize(enemy7Size / 10, enemy7Size / 10)
+        score += 1
+    if enemy8.ycor() <= -320 - enemy8Size:
+        enemy8Size = random.randint(10, 30)
+        enemy8.goto(random.randint(-320 + enemy8Size, 320 - enemy8Size), random.randint(360 + enemy8Size, 1400 + enemy8Size))
+        enemy8.turtlesize(enemy8Size / 10, enemy8Size / 10)
+        score += 1
+    if enemy9.ycor() <= -320 - enemy9Size:
+        enemy9Size = random.randint(10, 30)
+        enemy9.goto(random.randint(-320 + enemy9Size, 320 - enemy9Size), random.randint(320 + enemy9Size, 1400 + enemy9Size))
+        enemy9.turtlesize(enemy9Size / 10, enemy9Size / 10)
+        score += 1
+    if enemy0.ycor() <= -320 - enemy0Size:
+        enemy0Size = random.randint(10, 30)
+        enemy0.goto(random.randint(-320 + enemy0Size, 320 - enemy0Size), random.randint(360 + enemy0Size, 1400 + enemy0Size))
+        enemy0.turtlesize(enemy0Size / 10, enemy0Size / 10)
+        score += 1
+
+def checkForCollision():
+    global player, enemy0Size, enemy1Size, enemy2Size, enemy3Size, enemy4Size, enemy5Size, enemy6Size, enemy7Size, enemy8Size, enemy9Size
+    if (player.xcor() - enemy1.xcor()) >= -20 - (enemy1Size) and (player.xcor() - enemy1.xcor()) <= 20 + (enemy1Size) and (player.ycor() - enemy1.ycor()) >= -20 - (enemy1Size) and (player.ycor() - enemy1.ycor()) <= 20 + (enemy1Size):return True
+    elif (player.xcor() - enemy2.xcor()) >= -20 - (enemy2Size) and (player.xcor() - enemy2.xcor()) <= 20 + (enemy2Size) and (player.ycor() - enemy2.ycor()) >= -20 - (enemy2Size) and (player.ycor() - enemy2.ycor()) <= 20 + (enemy2Size):return True
+    elif (player.xcor() - enemy3.xcor()) >= -20 - (enemy3Size) and (player.xcor() - enemy3.xcor()) <= 20 + (enemy3Size) and (player.ycor() - enemy3.ycor()) >= -20 - (enemy3Size) and (player.ycor() - enemy3.ycor()) <= 20 + (enemy3Size):return True
+    elif (player.xcor() - enemy4.xcor()) >= -20 - (enemy4Size) and (player.xcor() - enemy4.xcor()) <= 20 + (enemy4Size) and (player.ycor() - enemy4.ycor()) >= -20 - (enemy4Size) and (player.ycor() - enemy4.ycor()) <= 20 + (enemy4Size):return True
+    elif (player.xcor() - enemy5.xcor()) >= -20 - (enemy5Size) and (player.xcor() - enemy5.xcor()) <= 20 + (enemy5Size) and (player.ycor() - enemy5.ycor()) >= -20 - (enemy5Size) and (player.ycor() - enemy5.ycor()) <= 20 + (enemy5Size):return True
+    elif (player.xcor() - enemy6.xcor()) >= -20 - (enemy6Size) and (player.xcor() - enemy6.xcor()) <= 20 + (enemy6Size) and (player.ycor() - enemy6.ycor()) >= -20 - (enemy6Size) and (player.ycor() - enemy6.ycor()) <= 20 + (enemy6Size):return True
+    elif (player.xcor() - enemy7.xcor()) >= -20 - (enemy7Size) and (player.xcor() - enemy7.xcor()) <= 20 + (enemy7Size) and (player.ycor() - enemy7.ycor()) >= -20 - (enemy7Size) and (player.ycor() - enemy7.ycor()) <= 20 + (enemy7Size):return True
+    elif (player.xcor() - enemy8.xcor()) >= -20 - (enemy8Size) and (player.xcor() - enemy8.xcor()) <= 20 + (enemy8Size) and (player.ycor() - enemy8.ycor()) >= -20 - (enemy8Size) and (player.ycor() - enemy8.ycor()) <= 20 + (enemy8Size):return True
+    elif (player.xcor() - enemy9.xcor()) >= -20 - (enemy9Size) and (player.xcor() - enemy9.xcor()) <= 20 + (enemy9Size) and (player.ycor() - enemy9.ycor()) >= -20 - (enemy9Size) and (player.ycor() - enemy9.ycor()) <= 20 + (enemy9Size):return True
+    elif (player.xcor() - enemy0.xcor()) >= -20 - (enemy0Size) and (player.xcor() - enemy0.xcor()) <= 20 + (enemy0Size) and (player.ycor() - enemy0.ycor()) >= -20 - (enemy0Size) and (player.ycor() - enemy0.ycor()) <= 20 + (enemy0Size):return True
+    else:return False
+
+def playerMovement(speed):
+    global up, down, left, right
+    if up == 1:player.goto(player.xcor() + speed, player.ycor())
+    if down == 1:player.goto(player.xcor() - speed, player.ycor())
+    if left == 1:player.goto(player.xcor(), player.ycor() - speed)
+    if right == 1:player.goto(player.xcor(), player.ycor() + speed)
+    if player.xcor() > 290:player.goto(290, player.ycor())
+    if player.xcor() < -300:player.goto(-300, player.ycor())
+    if player.ycor() > 300:player.goto(player.xcor(), 300)
+    if player.ycor() < -290:player.goto(player.xcor(), -290)
+
+def playerRight():
+    global up
+    up = 1
+def playerLeft():
+    global down
+    down = 1
+def playerDown():
+    global left
+    left = 1
+def playerUp():
+    global right
+    right = 1
+
+def playerRightR():
+    global up
+    up = 0
+def playerLeftR():
+    global down
+    down = 0
+def playerDownR():
+    global left
+    left = 0
+def playerUpR():
+    global right
+    right = 0
+
+def die():
+    global closed
+    closed = 1
+    screen.bye()
+
+def death():
+    enemy1.hideturtle()
+    enemy2.hideturtle()
+    enemy3.hideturtle()
+    enemy4.hideturtle()
+    enemy5.hideturtle()
+    enemy6.hideturtle()
+    enemy7.hideturtle()
+    enemy8.hideturtle()
+    enemy9.hideturtle()
+    enemy0.hideturtle()
+    player.hideturtle()
+
+    player.goto(0,0)
+    player.color("#ffffff")
+    t = "Game Over!! Score: " + str(score) + "!"
+    player.write(t, align = "center", font = ["Arial", 30, "normal"])
+
+    screen.onkey(die, "space")
+    screen.onkey(reset, "r")
+
+    screen.update()
+
+player = turtle.Turtle()
+enemy1 = turtle.Turtle()
+enemy2 = turtle.Turtle()
+enemy3 = turtle.Turtle()
+enemy4 = turtle.Turtle()
+enemy5 = turtle.Turtle()
+enemy6 = turtle.Turtle()
+enemy7 = turtle.Turtle()
+enemy8 = turtle.Turtle()
+enemy9 = turtle.Turtle()
+enemy0 = turtle.Turtle()
+
+screen = turtle.Screen()
+screen.tracer(0)
+screen.setup(640, 640, 0, 0)
+screen.bgcolor("#444444")
+
+screen.onkeypress(playerUp, "w")
+screen.onkeypress(playerLeft, "a")
+screen.onkeypress(playerDown, "s")
+screen.onkeypress(playerRight, "d")
+
+screen.onkeyrelease(playerUpR, "w")
+screen.onkeyrelease(playerLeftR, "a")
+screen.onkeyrelease(playerDownR, "s")
+screen.onkeyrelease(playerRightR, "d")
+
+screen.onkeypress(playerUp, "Up")
+screen.onkeypress(playerLeft, "Left")
+screen.onkeypress(playerDown, "Down")
+screen.onkeypress(playerRight, "Right")
+
+screen.onkeyrelease(playerUpR, "Up")
+screen.onkeyrelease(playerLeftR, "Left")
+screen.onkeyrelease(playerDownR, "Down")
+screen.onkeyrelease(playerRightR, "Right")
+
+screen.listen()
+
+enemy0Size = 1
+enemy1Size = 1
+enemy2Size = 1
+enemy3Size = 1
+enemy4Size = 1
+enemy5Size = 1
+enemy6Size = 1
+enemy7Size = 1
+enemy8Size = 1
+enemy9Size = 1
+
+while True:
+    reset()
+    while h == False:
+        showTurtles()
+        moveEnemiesDown(round(z))
+        checkForMoveUp()
+        playerMovement(round(z / 2) + 5)
+        h = checkForCollision()
+        if h == True:
+            death()
+        screen.update()
+        time.sleep(1 / 60)
+        z += 0.01
+    while h == 1:
+        time.sleep(1 / 20)
+        if closed == 1:
+            break
+        try:
+            screen.update()
+        except:
+            pass
+    if closed == 1:
+        break
